c176030830bb061dd764d8ff07c7351dd7bf4175
ddmills <mills.dalton@gmail.com>
2017-03-31 21:07:57 -0500

    add some basic nav

---
+++ assets/scripts/MoveTo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour {
+
+  public Transform goal;
+  private NavMeshAgent agent;
+
+  void Start () {
+    this.agent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update () {
+    if (this.goal) {
+      this.agent.destination = goal.position;
+    }
+  }
+}
